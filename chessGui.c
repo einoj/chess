592803b87eff49ebcd62df831443de45b2f298a7
Eino Juhani Oltedal <e.j.oltedal@fys.uio.no>
2014-05-31 10:27:10 +0200

    removed unnecessary code. Unfortunatly it still segfaults on archlinux

--- chessGui.c
+++ chessGui.c
@@ -164,14 +164,11 @@ char *convertClipieceToGuiPiece(int cliPiece){
 	}
 
 }
-char *tall[64]={"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","63"};
 int drawGuiBoard(GtkLabel *labels[][8], int cliBoard[][8]) {
-		int i, j, p=0;
+		int i, j;
 		for (i = 0; i < 8; i++) {
 			for (j = 0; j < 8; j++) {
 				gtk_label_set_text(labels[i][j], convertClipieceToGuiPiece(cliBoard[i][j]));
-				//gtk_label_set_text(labelBoard[i][j], tall[p]);
-				p++;
 			}
 		}
 		return 0;
