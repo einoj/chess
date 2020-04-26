885169563c5e5c494dabb348845d9dcc53c263b1
Eino J. Oltedal <einoju@gmail.com>
2020-04-26 21:24:11 +0200

    cleanup whitespace

--- src/chessGUI.c
+++ src/chessGUI.c
@@ -24,150 +24,150 @@ extern void resetPassantArrays(void);
 
 int main (int argc, char *argv[])
 {
-	/*fill the board array with pieces*/
-	initBoard(board);
-	resetPassantArrays();
-	GtkWidget *window, *eventbox;
+    /*fill the board array with pieces*/
+    initBoard(board);
+    resetPassantArrays();
+    GtkWidget *window, *eventbox;
     GtkLabel *label;
-	gtk_init (&argc, &argv);
-	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW (window), "Chess board");
-  	gtk_container_set_border_width(GTK_CONTAINER(window), 5);
-	gtk_widget_set_size_request(window, 680,350);
-	//table = gtk_grid_new (8,8,TRUE);
-	table = gtk_grid_new ();
-  GtkStyleContext *context;
-  GtkCssProvider *provider = gtk_css_provider_new ();
-  gtk_css_provider_load_from_path (provider,
-    "style/chessGUI.css", NULL);
-  context = gtk_widget_get_style_context (table);
-  gtk_style_context_add_provider (context,
-                                            GTK_STYLE_PROVIDER(provider),
-                                            GTK_STYLE_PROVIDER_PRIORITY_USER);
+    gtk_init (&argc, &argv);
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW (window), "Chess board");
+    gtk_container_set_border_width(GTK_CONTAINER(window), 5);
+    gtk_widget_set_size_request(window, 680,350);
+    //table = gtk_grid_new (8,8,TRUE);
+    table = gtk_grid_new ();
+    GtkStyleContext *context;
+    GtkCssProvider *provider = gtk_css_provider_new ();
+    gtk_css_provider_load_from_path (provider,
+            "style/chessGUI.css", NULL);
+    context = gtk_widget_get_style_context (table);
+    gtk_style_context_add_provider (context,
+            GTK_STYLE_PROVIDER(provider),
+            GTK_STYLE_PROVIDER_PRIORITY_USER);
 
-  /*container for the labels of the gui board*/
-  GtkLabel *labelBoard[8][8];
-	/*one is larger to make the squares wider*/	
-	char *pieces[64] = { "♜", "♞", "♝","♛","♚","♝","♞","♜",
-						 "♟", "♟", "♟","♟","♟","♟","♟","♟",
-						 "", "", "", "", "", "", "", "",
-						 "", "", "", "", "", "", "", "",
-						 "", "", "", "", "", "", "", "",
-						 "", "", "", "", "", "", "", "",
-						 "♙", "♙", "♙","♙","♙","♙","♙","♙",
-						 "♖", "♘", "♗","♕","♔","♗","♘","♖"};
-	int i,j;
-	int p = 0;
-	int oddCol = 1;
-	int oddRow = 1;
-	for (i = 0; i < 8; i ++) {
-		for (j = 0; j < 8; j++) {
-			label = (GtkLabel *) gtk_label_new(pieces[p]);
-      /* set the size of the label to avoid that they are resized when there is no piece in the row */
-      gtk_widget_set_size_request((GtkWidget *) label, 56, 56);
-			/*put the label into the container for easy access when mocing pieces*/
-			labelBoard[i][j]=label;
-			eventbox = gtk_event_box_new();
+    /*container for the labels of the gui board*/
+    GtkLabel *labelBoard[8][8];
+    /*one is larger to make the squares wider*/	
+    char *pieces[64] = { "♜", "♞", "♝","♛","♚","♝","♞","♜",
+        "♟", "♟", "♟","♟","♟","♟","♟","♟",
+        "", "", "", "", "", "", "", "",
+        "", "", "", "", "", "", "", "",
+        "", "", "", "", "", "", "", "",
+        "", "", "", "", "", "", "", "",
+        "♙", "♙", "♙","♙","♙","♙","♙","♙",
+        "♖", "♘", "♗","♕","♔","♗","♘","♖"};
+    int i,j;
+    int p = 0;
+    int oddCol = 1;
+    int oddRow = 1;
+    for (i = 0; i < 8; i ++) {
+        for (j = 0; j < 8; j++) {
+            label = (GtkLabel *) gtk_label_new(pieces[p]);
+            /* set the size of the label to avoid that they are resized when there is no piece in the row */
+            gtk_widget_set_size_request((GtkWidget *) label, 56, 56);
+            /*put the label into the container for easy access when mocing pieces*/
+            labelBoard[i][j]=label;
+            eventbox = gtk_event_box_new();
             context = gtk_widget_get_style_context (eventbox);
-                    gtk_style_context_add_provider (context,
-                                            GTK_STYLE_PROVIDER(provider),
-                                            GTK_STYLE_PROVIDER_PRIORITY_USER);
-			if (oddRow) {
-				if (oddCol) {
+            gtk_style_context_add_provider (context,
+                    GTK_STYLE_PROVIDER(provider),
+                    GTK_STYLE_PROVIDER_PRIORITY_USER);
+            if (oddRow) {
+                if (oddCol) {
                     gtk_widget_set_name (eventbox, "lightbrown");
-					oddCol = 0;
-				} else {
+                    oddCol = 0;
+                } else {
                     gtk_widget_set_name (eventbox, "darkbrown");
-					oddCol = 1;
-				}
-			} else {
-				if (oddCol) {
+                    oddCol = 1;
+                }
+            } else {
+                if (oddCol) {
                     gtk_widget_set_name (eventbox, "darkbrown");
-					oddCol = 0;
-				} else {
+                    oddCol = 0;
+                } else {
                     gtk_widget_set_name (eventbox, "lightbrown");
-					oddCol = 1;
-				}
-			}
+                    oddCol = 1;
+                }
+            }
 
-			gtk_event_box_set_above_child(GTK_EVENT_BOX(eventbox),FALSE);
-			/*put label into eventbox*/
-			gtk_container_add(GTK_CONTAINER (eventbox), (GtkWidget *) label);
-			/*put eventbox into table*/
-      gtk_grid_attach((GtkGrid *) table, eventbox,j+1,i,1,1);
+            gtk_event_box_set_above_child(GTK_EVENT_BOX(eventbox),FALSE);
+            /*put label into eventbox*/
+            gtk_container_add(GTK_CONTAINER (eventbox), (GtkWidget *) label);
+            /*put eventbox into table*/
+            gtk_grid_attach((GtkGrid *) table, eventbox,j+1,i,1,1);
 
 
-			g_signal_connect(G_OBJECT (eventbox), "button_press_event",
-					G_CALLBACK (button_pressed), (gpointer) labelBoard);
+            g_signal_connect(G_OBJECT (eventbox), "button_press_event",
+                    G_CALLBACK (button_pressed), (gpointer) labelBoard);
 
-			gtk_widget_set_events(eventbox, GDK_BUTTON_PRESS_MASK);
+            gtk_widget_set_events(eventbox, GDK_BUTTON_PRESS_MASK);
             /*Dont need widget_realize
              * connect to a signal that will be called 
              * after the widget is realized automatically
              * in g_signal_connect(window)
-			gtk_widget_realize(eventbox);*/
+             gtk_widget_realize(eventbox);*/
 
-			p++;
-		}
-		oddRow = !oddRow;
-	}
-  /* add square row names */
-  sprintf(mnum,"%s","`");
-  for (i = 1; i < 9; i++) {
-    mnum[0]++; // mnums first char becomes 'a', then 'b', then 'c' etc.
-	  label = (GtkLabel *) gtk_label_new(mnum);
-    gtk_widget_set_size_request((GtkWidget *) label, 0, 30);
-    gtk_grid_attach((GtkGrid *) table,(GtkWidget *) label,i,9,1,1);
-  }
-  i = 8;
-  for (j = 0; j < 8; j++) {
-    sprintf(mnum,"%d",i--);
-	  label = (GtkLabel *) gtk_label_new(mnum);
-    gtk_widget_set_size_request((GtkWidget *) label, 30, 0);
-    gtk_grid_attach((GtkGrid *) table,(GtkWidget *) label,0,j,1,1);
-  }
+            p++;
+        }
+        oddRow = !oddRow;
+    }
+    /* add square row names */
+    sprintf(mnum,"%s","`");
+    for (i = 1; i < 9; i++) {
+        mnum[0]++; // mnums first char becomes 'a', then 'b', then 'c' etc.
+        label = (GtkLabel *) gtk_label_new(mnum);
+        gtk_widget_set_size_request((GtkWidget *) label, 0, 30);
+        gtk_grid_attach((GtkGrid *) table,(GtkWidget *) label,i,9,1,1);
+    }
+    i = 8;
+    for (j = 0; j < 8; j++) {
+        sprintf(mnum,"%d",i--);
+        label = (GtkLabel *) gtk_label_new(mnum);
+        gtk_widget_set_size_request((GtkWidget *) label, 30, 0);
+        gtk_grid_attach((GtkGrid *) table,(GtkWidget *) label,0,j,1,1);
+    }
 
-	/*make a horizontal pane*/
-	//hpane = gtk_grid_new(1,2,TRUE);
-	hpane = gtk_grid_new();
-	/*add the table to the horizontal pane*/
-  gtk_grid_attach((GtkGrid *) hpane, table, 0, 0, 1,1);
+    /*make a horizontal pane*/
+    //hpane = gtk_grid_new(1,2,TRUE);
+    hpane = gtk_grid_new();
+    /*add the table to the horizontal pane*/
+    gtk_grid_attach((GtkGrid *) hpane, table, 0, 0, 1,1);
 
-  /* create a vertical grid containing game info.
-   * the first  widget is shows the current player
-   * and the second widget is a textview showing last moves
-   */
-  infogrid = gtk_grid_new();
-  textview = gtk_text_view_new();
-  gtk_text_view_set_editable(GTK_TEXT_VIEW(textview), FALSE);
-  gtk_text_view_set_cursor_visible (GTK_TEXT_VIEW (textview), FALSE);
-  gtk_text_view_set_wrap_mode (GTK_TEXT_VIEW (textview), GTK_WRAP_WORD);
-  buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW (textview));
-  gtk_text_view_set_left_margin (GTK_TEXT_VIEW (textview), 10);
-  gtk_text_view_set_right_margin (GTK_TEXT_VIEW (textview), 10);
+    /* create a vertical grid containing game info.
+     * the first  widget is shows the current player
+     * and the second widget is a textview showing last moves
+     */
+    infogrid = gtk_grid_new();
+    textview = gtk_text_view_new();
+    gtk_text_view_set_editable(GTK_TEXT_VIEW(textview), FALSE);
+    gtk_text_view_set_cursor_visible (GTK_TEXT_VIEW (textview), FALSE);
+    gtk_text_view_set_wrap_mode (GTK_TEXT_VIEW (textview), GTK_WRAP_WORD);
+    buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW (textview));
+    gtk_text_view_set_left_margin (GTK_TEXT_VIEW (textview), 10);
+    gtk_text_view_set_right_margin (GTK_TEXT_VIEW (textview), 10);
 
-	/*add game info textview to the scroll window 
-   * and the scroll window and current player label to infogrid.
-   * Finally add the info grid to the hpane, right of the chess board.*/
-  scroll_win = gtk_scrolled_window_new (NULL, NULL);
-  gtk_widget_set_size_request(scroll_win, 250,415);
-  gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scroll_win),
-  GTK_POLICY_AUTOMATIC, GTK_POLICY_ALWAYS);
-  gtk_container_add(GTK_CONTAINER (scroll_win), textview);
-  gtk_grid_attach((GtkGrid *) infogrid, (GtkWidget *) scroll_win,0,1,1,1);
-	currentPlayer = (GtkLabel *) gtk_label_new("Current player: White");
-  gtk_grid_attach((GtkGrid *) infogrid, (GtkWidget *) currentPlayer,0,0,1,1);
-  gtk_grid_attach((GtkGrid *) hpane, (GtkWidget *) infogrid, 1,0,1,1);
+    /*add game info textview to the scroll window 
+     * and the scroll window and current player label to infogrid.
+     * Finally add the info grid to the hpane, right of the chess board.*/
+    scroll_win = gtk_scrolled_window_new (NULL, NULL);
+    gtk_widget_set_size_request(scroll_win, 250,415);
+    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scroll_win),
+            GTK_POLICY_AUTOMATIC, GTK_POLICY_ALWAYS);
+    gtk_container_add(GTK_CONTAINER (scroll_win), textview);
+    gtk_grid_attach((GtkGrid *) infogrid, (GtkWidget *) scroll_win,0,1,1,1);
+    currentPlayer = (GtkLabel *) gtk_label_new("Current player: White");
+    gtk_grid_attach((GtkGrid *) infogrid, (GtkWidget *) currentPlayer,0,0,1,1);
+    gtk_grid_attach((GtkGrid *) hpane, (GtkWidget *) infogrid, 1,0,1,1);
 
 
 
-	/*add table to window*/
-	gtk_container_add (GTK_CONTAINER (window), hpane);
-	gtk_widget_show_all (window);
-	g_signal_connect_swapped(G_OBJECT(window), "destroy",
-		G_CALLBACK(gtk_main_quit), NULL);
-	gtk_main();
-	return 0;
+    /*add table to window*/
+    gtk_container_add (GTK_CONTAINER (window), hpane);
+    gtk_widget_show_all (window);
+    g_signal_connect_swapped(G_OBJECT(window), "destroy",
+            G_CALLBACK(gtk_main_quit), NULL);
+    gtk_main();
+    return 0;
 }
 
 
@@ -211,74 +211,74 @@ void algebraic_notation(char *note, int *move, int board[][8])
 static gboolean button_pressed (GtkWidget *ebox, GdkEventButton *event,
 			GtkLabel *labelBoard[][8])
 {
-   // prevEventbox = eventbox;// Just set the prevEventbox to avoid nullpointer exception
-	unsigned left, top, width, height;
+    // prevEventbox = eventbox;// Just set the prevEventbox to avoid nullpointer exception
+    unsigned left, top, width, height;
 
-	if (event->type == GDK_BUTTON_PRESS)
-	{
-		if (!clicks) {
+    if (event->type == GDK_BUTTON_PRESS)
+    {
+        if (!clicks) {
             gtk_widget_set_name (ebox, "selected");
-			/*get coordinates of eventbox*/
-			gtk_container_child_get(GTK_CONTAINER(table), ebox,
-					"left-attach", &left,
-					"top-attach",&top,
-					"width",&width,
-					"height",&height,
-					NULL);
-      //store the position you move from. It will be used to move the pieces in the board array
-      // subtract 1 from left because the row numbers are part of the label table
-      move[0] = left-1;
-      move[1] = top;
-			/*save label*/
-			prevEventbox = ebox;
-			/*save the current coordinates*/
-			clicks = 1;
-		} else {
-			/*make move*/
-			gtk_container_child_get(GTK_CONTAINER(table), ebox,
-					"left-attach",  &left,
-					"top-attach",   &top,
-					"width", &width,
-					"height",&height,
-					NULL);
-      //store the position you move to. It will be used to move the pieces in the board array
-      // subtract 1 from left because the row numbers are part of the label table
-      move[2] = left-1;
-      move[3] = top;
-			/*color back to normal*/
-			if ((move[0]+move[1])&1){
-				/*odd square, darkbrown color*/
-				//gtk_widget_override_background_color(prevEventbox, GTK_STATE_NORMAL, &dbrown);
+            /*get coordinates of eventbox*/
+            gtk_container_child_get(GTK_CONTAINER(table), ebox,
+                    "left-attach", &left,
+                    "top-attach",&top,
+                    "width",&width,
+                    "height",&height,
+                    NULL);
+            //store the position you move from. It will be used to move the pieces in the board array
+            // subtract 1 from left because the row numbers are part of the label table
+            move[0] = left-1;
+            move[1] = top;
+            /*save label*/
+            prevEventbox = ebox;
+            /*save the current coordinates*/
+            clicks = 1;
+        } else {
+            /*make move*/
+            gtk_container_child_get(GTK_CONTAINER(table), ebox,
+                    "left-attach",  &left,
+                    "top-attach",   &top,
+                    "width", &width,
+                    "height",&height,
+                    NULL);
+            //store the position you move to. It will be used to move the pieces in the board array
+            // subtract 1 from left because the row numbers are part of the label table
+            move[2] = left-1;
+            move[3] = top;
+            /*color back to normal*/
+            if ((move[0]+move[1])&1){
+                /*odd square, darkbrown color*/
+                //gtk_widget_override_background_color(prevEventbox, GTK_STATE_NORMAL, &dbrown);
                 gtk_widget_set_name (prevEventbox, "darkbrown"); 
-			} else {
-				/*even square, lightbrown color*/
+            } else {
+                /*even square, lightbrown color*/
                 gtk_widget_set_name (prevEventbox, "lightbrown"); 
-			}
-      algebraic_notation(note, move, board);
-			int u = makemove(player, move, board);
-			if (!u) {
-        buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW (textview));
-        gtk_text_buffer_get_end_iter(buffer, &txtiter);
-        if (!player) {
-          sprintf(mnum,"%d",++movecnt);
-          gtk_text_buffer_insert(buffer, &txtiter, " ", -1);
-          gtk_text_buffer_insert(buffer, &txtiter, mnum, -1);
-          gtk_text_buffer_insert(buffer, &txtiter, ".", -1);
-        }
-        /* Update the GUI board */
-				drawGuiBoard(labelBoard, board);	
-        /* Update the game info */
-        gtk_text_buffer_insert(buffer, &txtiter, note, -1);
+            }
+            algebraic_notation(note, move, board);
+            int u = makemove(player, move, board);
+            if (!u) {
+                buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW (textview));
+                gtk_text_buffer_get_end_iter(buffer, &txtiter);
+                if (!player) {
+                    sprintf(mnum,"%d",++movecnt);
+                    gtk_text_buffer_insert(buffer, &txtiter, " ", -1);
+                    gtk_text_buffer_insert(buffer, &txtiter, mnum, -1);
+                    gtk_text_buffer_insert(buffer, &txtiter, ".", -1);
+                }
+                /* Update the GUI board */
+                drawGuiBoard(labelBoard, board);	
+                /* Update the game info */
+                gtk_text_buffer_insert(buffer, &txtiter, note, -1);
 
-				player = !player; 
-				if (player) {
-					gtk_label_set_text(currentPlayer, "Current player: Black");
-				} else {
-					gtk_label_set_text(currentPlayer, "Current player: White");
-				}
-			}
-			clicks = 0;
-		}
+                player = !player; 
+                if (player) {
+                    gtk_label_set_text(currentPlayer, "Current player: Black");
+                } else {
+                    gtk_label_set_text(currentPlayer, "Current player: White");
+                }
+            }
+            clicks = 0;
         }
-	return FALSE;
+    }
+    return FALSE;
 }
