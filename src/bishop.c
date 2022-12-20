c05bae6cfc61efc6c4e1572b59ccffb687b50132
Eino J. Oltedal <einoju@gmail.com>
2022-12-20 23:17:43 +0100

    bishop.c: Fix bug with movement
    I had confused rows and columns. It is probably to refactor how moves
    are stored. Maybe instead of using a array of positions setup a struct
    containing current row/column and next row/column with good names
    instead of using magic indices in an array.

--- src/bishop.c
+++ src/bishop.c
@@ -7,74 +7,74 @@ int bishop(int pos[], int board[][8]) {
 	/*pos[] contains current possitions and new possitions [0][1]=current [2][3] = new*/
 	int i,j;
 	if (pos[3] > pos[1] && pos[2] > pos[0]) {
-		/*move north east*/
+		/*move down right*/
 		/*if move is not horizontal, return 0*/
 		/*change in x-axis must equal change in y-axis*/
 		if (!(abs(pos[0]-pos[2])==abs(pos[1]-pos[3]))){
 			return 0;
 		}
-		j = pos[1];
-		for(i = pos[0]+1; i < pos[2]; i++) {
+		j = pos[0];
+		for(i = pos[1]+1; i < pos[3]; i++) {
 				j++;
 				if (checkPosition(i,j,board)) {
 					/*if piece at intermediate possition move is illegal*/
 					return 0;
 				}
-			}
+		}
 		/*reached new position*/
 		return 1;
 	}
 	else if (pos[3] > pos[1] && pos[2] < pos[0]) {
-		/*move north west*/
+		/*move down left*/
 		/*if move is not horizontal, return 0*/
 		/*change in x-axis must equal change in y-axis*/
 		if (!(abs(pos[0]-pos[2])==abs(pos[1]-pos[3]))){
 			return 0;
 		}
-		j = pos[1];
-		for(i = pos[0]-1; i > pos[2]; i--) {
+		j = pos[0];
+		for(i = pos[1]+1; i < pos[3]; i++) {
+				j--;
+				if (checkPosition(i,j,board)) {
+					/*if piece at intermediate possition move is illegal*/
+					return 0;
+				}
+		}
+		/*reached new position*/
+		return 1;
+	}
+	else if (pos[3] < pos[1] && pos[2] < pos[0]) {
+		/*move up left*/
+		/*if move is not horizontal, return 0*/
+		/*change in x-axis must equal change in y-axis*/
+		if (!(abs(pos[0]-pos[2])==abs(pos[1]-pos[3]))){
+			return 0;
+		}
+		j = pos[0];
+		for(i = pos[1]-1; i > pos[3]; i--) {
+				j--;
+				if (checkPosition(i,j,board)) {
+					/*if piece at intermediate possition move is illegal*/
+					return 0;
+				}
+		}
+		/*reached new position*/
+		return 1;
+	}
+	else if (pos[3] < pos[1] && pos[2] > pos[0]) {
+		/*move up right*/
+		/*if move is not horizontal, return 0*/
+		/*change in x-axis must equal change in y-axis*/
+		if (!(abs(pos[0]-pos[2])==abs(pos[1]-pos[3]))){
+			return 0;
+		}
+		j = pos[0];
+		for(i = pos[1]-1; i > pos[3]; i--) {
 				j++;
 				if (checkPosition(i,j,board)) {
 					/*if piece at intermediate possition move is illegal*/
 					return 0;
 				}
-			}
-		/*reached new position*/
-		return 1;
-	}
-	else if (pos[3] < pos[1] && pos[2] < pos[0]) {
-		/*move south west*/
-		/*if move is not horizontal, return 0*/
-		/*change in x-axis must equal change in y-axis*/
-		if (!(abs(pos[0]-pos[2])==abs(pos[1]-pos[3]))){
-			return 0;
 		}
-		j = pos[1];
-		for(i = pos[0]-1; i > pos[2]; i--) {
-				--j;
-				if (checkPosition(i,j,board)) {
-					/*if piece at intermediate possition move is illegal*/
-					return 0;
-				}
-			}
-		/*reached new position*/
-		return 1;
-	}
-	else if (pos[3] < pos[1] && pos[2] > pos[0]) {
-		/*move south east*/
-		/*if move is not horizontal, return 0*/
-		/*change in x-axis must equal change in y-axis*/
-		if (!(abs(pos[0]-pos[2])==abs(pos[1]-pos[3]))){
-			return 0;
-		}
-		j = pos[1];
-		for(i = pos[0]+1; i < pos[2]; i++) {
-				--j;
-				if (checkPosition(i,j,board)) {
-					/*if piece at intermediate possition move is illegal*/
-					return 0;
-				}
-			}
 		/*reached new position*/
 		return 1;
 	}
