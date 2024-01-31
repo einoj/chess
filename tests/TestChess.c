377628f1e68913362a88f41e9c0de68b2abd6c7b
Eino Juhani Oltedal <einoju@gmail.com>
2024-01-31 16:00:17 +0100

    Add a test game Ganguly-Madaminov WC Rapid 2023
    So There is something more to test regression test against.

--- tests/TestChess.c
+++ tests/TestChess.c
@@ -2,9 +2,11 @@
 #include "letterToInt.h"
 #include "unity.h"
 
+static int board[8][8];
 void setUp(void)
 {
   /* This is run before EACH TEST */
+  initBoard(board);
 }
 
 void tearDown(void)
@@ -13,8 +15,6 @@ void tearDown(void)
 
 void test_d2d4_opening(void)
 {
-	int board[8][8];
-	initBoard(board);
     struct Move w_mov = {
         .currCol = ltoi('d'), .currRow = ltoi('2'), .nextCol = ltoi('d'), .nextRow = ltoi('4')
     };
@@ -36,10 +36,58 @@ void test_d2d4_opening(void)
     TEST_ASSERT_EQUAL_MEMORY(expected_board, board, 64);
 }
 
+void test_scholars_mate_wc2023_gangulyVmadaminow(void)
+{
+    struct Move w_mov = {.currCol = ltoi('e'), .currRow = ltoi('2'), .nextCol = ltoi('e'), .nextRow = ltoi('4')};
+    struct Move b_mov = {.currCol = ltoi('e'), .currRow = ltoi('7'), .nextCol = ltoi('e'), .nextRow = ltoi('5')};
+    TEST_ASSERT_EQUAL_MESSAGE(0, makemove(0, w_mov, board), "White Move");
+    TEST_ASSERT_EQUAL_MESSAGE(0, makemove(1, b_mov, board), "Black Move");
+    w_mov = (struct Move) {.currCol = ltoi('g'), .currRow = ltoi('1'), .nextCol = ltoi('f'), .nextRow = ltoi('3')};
+    b_mov = (struct Move) {.currCol = ltoi('b'), .currRow = ltoi('8'), .nextCol = ltoi('c'), .nextRow = ltoi('6')};
+    TEST_ASSERT_EQUAL_MESSAGE(0, makemove(0, w_mov, board), "White Move");
+    TEST_ASSERT_EQUAL_MESSAGE(0, makemove(1, b_mov, board), "Black Move");
+    w_mov = (struct Move) {.currCol = ltoi('d'), .currRow = ltoi('2'), .nextCol = ltoi('d'), .nextRow = ltoi('4')};
+    b_mov = (struct Move) {.currCol = ltoi('e'), .currRow = ltoi('5'), .nextCol = ltoi('d'), .nextRow = ltoi('4')};
+    TEST_ASSERT_EQUAL_MESSAGE(0, makemove(0, w_mov, board), "White Move");
+    TEST_ASSERT_EQUAL_MESSAGE(0, makemove(1, b_mov, board), "Black Move");
+    w_mov = (struct Move) {.currCol = ltoi('f'), .currRow = ltoi('3'), .nextCol = ltoi('d'), .nextRow = ltoi('4')};
+    b_mov = (struct Move) {.currCol = ltoi('d'), .currRow = ltoi('8'), .nextCol = ltoi('f'), .nextRow = ltoi('6')};
+    TEST_ASSERT_EQUAL_MESSAGE(0, makemove(0, w_mov, board), "White Move");
+    TEST_ASSERT_EQUAL_MESSAGE(0, makemove(1, b_mov, board), "Black Move");
+    w_mov = (struct Move) {.currCol = ltoi('d'), .currRow = ltoi('4'), .nextCol = ltoi('f'), .nextRow = ltoi('3')};
+    b_mov = (struct Move) {.currCol = ltoi('f'), .currRow = ltoi('8'), .nextCol = ltoi('b'), .nextRow = ltoi('4')};
+    TEST_ASSERT_EQUAL_MESSAGE(0, makemove(0, w_mov, board), "White Move");
+    TEST_ASSERT_EQUAL_MESSAGE(0, makemove(1, b_mov, board), "Black Move");
+    w_mov = (struct Move) {.currCol = ltoi('c'), .currRow = ltoi('2'), .nextCol = ltoi('c'), .nextRow = ltoi('3')};
+    b_mov = (struct Move) {.currCol = ltoi('b'), .currRow = ltoi('4'), .nextCol = ltoi('c'), .nextRow = ltoi('5')};
+    TEST_ASSERT_EQUAL_MESSAGE(0, makemove(0, w_mov, board), "White Move");
+    TEST_ASSERT_EQUAL_MESSAGE(0, makemove(1, b_mov, board), "Black Move");
+    w_mov = (struct Move) {.currCol = ltoi('f'), .currRow = ltoi('1'), .nextCol = ltoi('d'), .nextRow = ltoi('3')};
+    b_mov = (struct Move) {.currCol = ltoi('c'), .currRow = ltoi('6'), .nextCol = ltoi('e'), .nextRow = ltoi('5')};
+    TEST_ASSERT_EQUAL_MESSAGE(0, makemove(0, w_mov, board), "White Move");
+    TEST_ASSERT_EQUAL_MESSAGE(0, makemove(1, b_mov, board), "Black Move");
+    w_mov = (struct Move) {.currCol = ltoi('f'), .currRow = ltoi('3'), .nextCol = ltoi('e'), .nextRow = ltoi('5')};
+    b_mov = (struct Move) {.currCol = ltoi('f'), .currRow = ltoi('6'), .nextCol = ltoi('e'), .nextRow = ltoi('5')};
+    TEST_ASSERT_EQUAL_MESSAGE(0, makemove(0, w_mov, board), "White Move");
+    TEST_ASSERT_EQUAL_MESSAGE(0, makemove(1, b_mov, board), "Black Move");
+    int expected_board[8][8] = {
+        {bRook, 0, bBishop, 0, bKing, 0, bKnight, bRook},
+        {bPawn, bPawn, bPawn, bPawn, 0, bPawn, bPawn, bPawn},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, bBishop, 0, wKnight, 0, 0, 0},
+        {0, 0, 0, 0, wPawn, 0, 0, 0},
+        {0, 0, wPawn, wBishop, 0, 0, 0, 0},
+        {wPawn, wPawn, 0, 0, 0, bQueen, wPawn, wPawn},
+        {wRook, wKnight, wBishop, wQueen, wKing, 0, 0, wRook}
+    };
+    TEST_ASSERT_EQUAL_MEMORY(expected_board, board, 64);
+}
+
 int main(void)
 {
   UnityBegin("test/TestProductionCode.c");
   RUN_TEST(test_d2d4_opening, 21);
+  RUN_TEST(test_scholars_mate_wc2023_gangulyVmadaminow, 22);
 
   return (UnityEnd());
 }
