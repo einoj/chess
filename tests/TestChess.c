260b5277409541edacdd59fcff76bebcfd30440c
Eino J. Oltedal <einoju@gmail.com>
2024-03-16 11:11:05 +0100

    TestChess: Check that makemove does not return 0
    Since moving a pawn can now return 2 or 3 even if the move is valid,
    because of handling of pawn promotion, the tests need to check that the
    return value is not 0 instead of checking that it equals 1.

--- tests/TestChess.c
+++ tests/TestChess.c
@@ -21,8 +21,8 @@ void test_d2d4_opening(void)
     struct Move b_mov = {
         .currCol = ltoi('d'), .currRow = ltoi('7'), .nextCol = ltoi('d'), .nextRow = ltoi('5')
     };
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(0, w_mov, board), "White Move");
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(1, b_mov, board), "Black Move");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(0, w_mov, board), "White Move");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(1, b_mov, board), "Black Move");
     int expected_board[8][8] = {
         { bRook, bKnight, bBishop, bQueen, bKing, bBishop, bKnight, bRook },
         { bPawn, bPawn, bPawn, 0, bPawn, bPawn, bPawn, bPawn },
@@ -42,50 +42,50 @@ void test_scholars_mate_wc2023_gangulyVmadaminow(void)
     struct Move b_mov = { .currCol = ltoi('e'), .currRow = ltoi('7'), .nextCol = ltoi('e'), .nextRow = ltoi('5') };
     TEST_ASSERT_EQUAL(1, checkInput(w_mov));
     TEST_ASSERT_EQUAL(1, checkInput(b_mov));
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(0, w_mov, board), "White Move");
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(1, b_mov, board), "Black Move");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(0, w_mov, board), "White Move");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(1, b_mov, board), "Black Move");
     w_mov = (struct Move) { .currCol = ltoi('g'), .currRow = ltoi('1'), .nextCol = ltoi('f'), .nextRow = ltoi('3') };
     b_mov = (struct Move) { .currCol = ltoi('b'), .currRow = ltoi('8'), .nextCol = ltoi('c'), .nextRow = ltoi('6') };
     TEST_ASSERT_EQUAL(1, checkInput(w_mov));
     TEST_ASSERT_EQUAL(1, checkInput(b_mov));
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(0, w_mov, board), "White Move");
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(1, b_mov, board), "Black Move");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(0, w_mov, board), "White Move");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(1, b_mov, board), "Black Move");
     w_mov = (struct Move) { .currCol = ltoi('d'), .currRow = ltoi('2'), .nextCol = ltoi('d'), .nextRow = ltoi('4') };
     b_mov = (struct Move) { .currCol = ltoi('e'), .currRow = ltoi('5'), .nextCol = ltoi('d'), .nextRow = ltoi('4') };
     TEST_ASSERT_EQUAL(1, checkInput(w_mov));
     TEST_ASSERT_EQUAL(1, checkInput(b_mov));
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(0, w_mov, board), "White Move");
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(1, b_mov, board), "Black Move");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(0, w_mov, board), "White Move");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(1, b_mov, board), "Black Move");
     w_mov = (struct Move) { .currCol = ltoi('f'), .currRow = ltoi('3'), .nextCol = ltoi('d'), .nextRow = ltoi('4') };
     b_mov = (struct Move) { .currCol = ltoi('d'), .currRow = ltoi('8'), .nextCol = ltoi('f'), .nextRow = ltoi('6') };
     TEST_ASSERT_EQUAL(1, checkInput(w_mov));
     TEST_ASSERT_EQUAL(1, checkInput(b_mov));
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(0, w_mov, board), "White Move");
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(1, b_mov, board), "Black Move");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(0, w_mov, board), "White Move");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(1, b_mov, board), "Black Move");
     w_mov = (struct Move) { .currCol = ltoi('d'), .currRow = ltoi('4'), .nextCol = ltoi('f'), .nextRow = ltoi('3') };
     b_mov = (struct Move) { .currCol = ltoi('f'), .currRow = ltoi('8'), .nextCol = ltoi('b'), .nextRow = ltoi('4') };
     TEST_ASSERT_EQUAL(1, checkInput(w_mov));
     TEST_ASSERT_EQUAL(1, checkInput(b_mov));
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(0, w_mov, board), "White Move");
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(1, b_mov, board), "Black Move");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(0, w_mov, board), "White Move");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(1, b_mov, board), "Black Move");
     w_mov = (struct Move) { .currCol = ltoi('c'), .currRow = ltoi('2'), .nextCol = ltoi('c'), .nextRow = ltoi('3') };
     b_mov = (struct Move) { .currCol = ltoi('b'), .currRow = ltoi('4'), .nextCol = ltoi('c'), .nextRow = ltoi('5') };
     TEST_ASSERT_EQUAL(1, checkInput(w_mov));
     TEST_ASSERT_EQUAL(1, checkInput(b_mov));
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(0, w_mov, board), "White Move");
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(1, b_mov, board), "Black Move");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(0, w_mov, board), "White Move");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(1, b_mov, board), "Black Move");
     w_mov = (struct Move) { .currCol = ltoi('f'), .currRow = ltoi('1'), .nextCol = ltoi('d'), .nextRow = ltoi('3') };
     b_mov = (struct Move) { .currCol = ltoi('c'), .currRow = ltoi('6'), .nextCol = ltoi('e'), .nextRow = ltoi('5') };
     TEST_ASSERT_EQUAL(1, checkInput(w_mov));
     TEST_ASSERT_EQUAL(1, checkInput(b_mov));
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(0, w_mov, board), "White Move");
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(1, b_mov, board), "Black Move");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(0, w_mov, board), "White Move");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(1, b_mov, board), "Black Move");
     w_mov = (struct Move) { .currCol = ltoi('f'), .currRow = ltoi('3'), .nextCol = ltoi('e'), .nextRow = ltoi('5') };
     b_mov = (struct Move) { .currCol = ltoi('f'), .currRow = ltoi('6'), .nextCol = ltoi('e'), .nextRow = ltoi('5') };
     TEST_ASSERT_EQUAL(1, checkInput(w_mov));
     TEST_ASSERT_EQUAL(1, checkInput(b_mov));
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(0, w_mov, board), "White Move");
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(1, b_mov, board), "Black Move");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(0, w_mov, board), "White Move");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(1, b_mov, board), "Black Move");
     int expected_board[8][8] = {
         { bRook, 0, bBishop, 0, bKing, 0, bKnight, bRook },
         { bPawn, bPawn, bPawn, bPawn, 0, bPawn, bPawn, bPawn },
@@ -151,7 +151,7 @@ void test_king_cannot_move_to_square_uder_attack(void)
     struct Move mov = { .currCol = ltoi('d'), .currRow = ltoi('5'), .nextCol = ltoi('d'), .nextRow = ltoi('6') };
     TEST_ASSERT_EQUAL_MESSAGE(0, makemove(white, mov, custom_board), "White king moved to d6 which is under attack");
     mov = (struct Move) { .currCol = ltoi('d'), .currRow = ltoi('5'), .nextCol = ltoi('c'), .nextRow = ltoi('5') };
-    TEST_ASSERT_EQUAL_MESSAGE(1, makemove(white, mov, custom_board), "White king can't move to d4.");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(0, makemove(white, mov, custom_board), "White king can't move to d4.");
     mov = (struct Move) { .currCol = ltoi('c'), .currRow = ltoi('5'), .nextCol = ltoi('c'), .nextRow = ltoi('6') };
     TEST_ASSERT_EQUAL_MESSAGE(0, makemove(white, mov, custom_board), "White king moved to c4, which is under attack by a pawn");
 }
