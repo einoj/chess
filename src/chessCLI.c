486099b27deb44744f2c5486ff0082ba30411a95
Eino J. Oltedal <einoju@gmail.com>
2024-03-17 11:13:39 +0100

    chessCLI: Implement pawn promotion
    Since the player only inputs a single character for the promotion input
    I had to make a conversion function to convert between the input and the
    colored pieces. Maybe the pawnPromotion function can be refactored to
    handle the color conversion, but that can be done later.

--- src/chessCLI.c
+++ src/chessCLI.c
@@ -30,15 +30,71 @@ int getInput(int player, struct Move* mov, int board[][8])
     return 0;
 }
 
+int getPromotionInput(int *input)
+{
+    printf("Promote pawn to: [N, B, R, Q]");
+    *input = fgetc(stdin);
+    if (input == NULL) {
+        return -1;
+        printf("ERROR: Invalid input\n");
+    }
+    if (*input == 'N' || *input == 'B' || *input  == 'R' || *input == 'Q')
+        return 0;
+    return -1;
+}
+
+int convertPromotionInput(int *input, int player)
+{
+    switch (*input) {
+    case 'N':
+        if (player == white)
+            *input = wKnight;
+        else if (player == black)
+            *input = bKnight;
+        return 1;
+    case 'B':
+        if (player == white)
+            *input = wBishop;
+        else if (player == black)
+            *input = bBishop;
+        return 1;
+    case 'R':
+        if (player == white)
+            *input = wRook;
+        else if (player == black)
+            *input = bRook;
+        return 1;
+    case 'Q':
+        if (player == white)
+            *input = wQueen;
+        else if (player == black)
+            *input = bQueen;
+        return 1;
+    }
+    return -1;
+}
+
 void playerLoop(int player, int board[][8])
 {
     struct Move mov;
+    struct Square sq;
+    int ret;
+    int promote_to_piece;
     printf(CLEAR);
     printBoard(board);
     do {
         while (getInput(player, &mov, board))
             ;
-    } while (!makemove(player, mov, board));
+       ret = makemove(player, mov, board);
+       if (ret == 3) {
+           sq.row=mov.nextRow;
+           sq.col=mov.nextCol;
+           while(getPromotionInput(&promote_to_piece))
+               ;
+           convertPromotionInput(&promote_to_piece, player);
+           promotePawn(player, sq, promote_to_piece, board);
+       }
+    } while (!ret);
 }
 
 int main(void)
